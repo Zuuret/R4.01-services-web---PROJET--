bff84fcc9e32c8bf786254110fbae742bb04680d
EnzoVidy <enzo.vidy1@gmail.com>
2025-02-20 11:20:13 +0100

    projet 2 reset

--- projet_2/config/auth.config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    //vous pouvez modifier cette valeur
-    secret: process.env.AUTH_SECRET || "darth-vader",
-    jwtExpiration: 60, // 1 minute
-    jwtRefreshExpiration: 120, // 2 minutes
-};
